d89bf1e919280478db911184e590d3908c53261e
Binh Hoang <hoangb3299@my.sduhsd.net>
2026-04-06 21:11:07 +0000

    define radio buttons for level and play btns

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// add javascript here
+
+// select level radio buttons
+let selected = document.querySelector('input[name="level"]:checked');
+//select "play" button
+const btn = document.getElementById("playBtn");
+btn.addEventListener("click", beginPlay());
+ 
+// play button function
+function beginPlay(){
+
+// easy level
+if(document.getElementsByName('e').checked){
+    let range = 3;
+    Math.floor(Math.random())*range + 1; // generates the number
+    document.querySelector('e');}
+}
